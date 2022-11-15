1cd0e4f92fb60556317c2d88e5b1343dffd7c703
BT_4N2_12 <tdukdak9287@bths.edu>
2022-11-15 12:29:31 -0500

    started coding two diff case choices

--- src/FalloutLawyer.java
+++ src/FalloutLawyer.java
@@ -27,21 +27,32 @@ public class FalloutLawyer
     {
         String goodStats = "";
         int count = (s + p + e + c + i + a + l);
-        if (count == 28)
+        if (count == 35)
         {
             goodStats = "SPECIAL Stats:" + "\nStrength: " + s + "\nPerception: " + p + "\nEndurance: " + e + "\nCharisma: " + c + "\nIntelligence: " + i + "\nAgility: " + a + "\nLuck: " + l;
         }
-        while (count != 28)
+        while (count != 35)
         {
-            if (count != 28)
+            if (count != 35)
             {
                 goodStats = "Your current stats are not equal to the number of SPECIAL points given. Your current stats are: \nStrength:" + s + "\nPerception: " + p + "\nEndurance: " + "\nCharisma:" + c + "\nIntelligence: " + i + "\nAgility: " + a + "\nLuck: " + l;
             }
         }
         return goodStats;
     }
-    //need to add way to replace stats if count != 28, loop as it is now is infinite
+    //need to add way to replace stats if count != 35, loop as it is now is infinite
 
-    public String caseName = "Dweller v Gray";
-
+    public String caseDetail(int caseNum)
+    {
+        String caseDet = "";
+        if (caseNum == 1)
+        {
+            caseDet = "\nCase: Dweller v Moreau\nFacts of the Case: The Vault Dweller brings to the defendant Richard Moreau a claim of the Unity project, a plan to expose all remaining human survivors to the Forced Evolutionary Virus to turn them into super mutants united in a hivemind under Mr. Moreau, being an unreasonable and unjust plan for the future of the Wasteland.\nIssue: Will the Unity provide humanity with the best plan for survival in the wake of apocalypse?\nCourt Opinion: Undecided";
+        }
+        if (caseNum == 2)
+        {
+            caseDet = "\nCase: House v Courier\nFacts of the Case: Robert House";
+        }
+        return caseDet;
+    }
 }
--- src/LawyerRunner.java
+++ src/LawyerRunner.java
@@ -5,6 +5,7 @@ public class LawyerRunner
     public static void main(String[] args)
     {
         FalloutLawyer dweller = new FalloutLawyer();
+        //FalloutLawyer house = new FalloutLawyer(); (in case i want to add 2 possible cases)
 
         Scanner n = new Scanner(System.in);
         Scanner s = new Scanner(System.in);
@@ -14,11 +15,14 @@ public class LawyerRunner
         Scanner i = new Scanner(System.in);
         Scanner a = new Scanner(System.in);
         Scanner l = new Scanner(System.in);
+        Scanner c2 = new Scanner(System.in);
+        //scanning for name, all SPECIAL stats, and case choice
 
         System.out.print("Enter name: ");
         String name = n.nextLine();
+        //places name of lawyer into String variable
 
-        System.out.print("Enter SPECIAL stats (remember, you only have 28 SPECIAL points available)\nEnter your strength stat: ");
+        System.out.print("Enter SPECIAL stats (remember, you only have 35 SPECIAL points available)\nEnter your strength stat: ");
         int strength = s.nextInt();
         System.out.print("Enter you perception stat: ");
         int perception = p.nextInt();
@@ -30,10 +34,14 @@ public class LawyerRunner
         int intelligence = i.nextInt();
         System.out.print("Enter your agility stat: ");
         int agility = a.nextInt();
-        System.out.print("Luck: ");
+        System.out.print("Enter your luck stat: ");
         int luck = l.nextInt();
+        //places each SPECIAL stat into an integer variable
+        System.out.println("Would you like to view case 1 or case 2?: ");
+        int caseChoice = c2.nextInt();
 
         System.out.println("\nName: " + dweller.lawyerName(name));
-        System.out.print(dweller.statChoice(strength,perception,endurance,charisma,intelligence,agility,luck));
+        System.out.println(dweller.statChoice(strength,perception,endurance,charisma,intelligence,agility,luck));
+        System.out.print(dweller.caseDetail(caseChoice));
     }
 }
